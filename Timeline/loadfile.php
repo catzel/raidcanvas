f145f38d5eb71d56199993256b908de0d1203074
catzel <sknimitz@gmail.com>
2020-01-28 17:50:48 -0500

    JSON test. I should set up Apache.

--- Timeline/loadfile.php
+++ Timeline/loadfile.php
@@ -1,5 +1,6 @@
 <?php
-$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+$id = $_POST['id'];
+$arr = array('a' => $id, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 
 echo json_encode($arr);
 ?>

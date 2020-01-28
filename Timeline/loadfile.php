25914d233ace2d2b4233404e71ed26ebb2fa7859
catzel <sknimitz@gmail.com>
2020-01-28 18:31:17 -0500

    Load file php logic.

--- Timeline/loadfile.php
+++ Timeline/loadfile.php
@@ -1,6 +1,34 @@
 <?php
-$id = $_POST['id'];
-$arr = array('a' => $id, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+$guid = $_POST['guid'];
 
-echo json_encode($arr);
+if (empty($guid)) {
+  echo json_encode(array('success' => false, 'message' => "No ID was provided."));
+  trigger_error("No ID was provided.", E_ERROR);
+}
+
+$filename = "/Repository/$guid.json";
+
+if (file_exists($filename)) {
+  $string = file_get_contents($filename);
+  $parseError = false;
+  if (empty($string)) {
+    $parseError = true;
+  } else {
+    $json = json_decode($string);
+    if ($json && $string != $json) {
+      echo $string;
+    } else {
+      $parseError = true;
+    }
+  }
+
+  if ($parseError) {
+    echo json_encode(array('success' => false, 'message' => "File is empty or invalid."));
+    trigger_error("File is empty or invalid.", E_ERROR);
+  }
+} else {
+  echo json_encode(array('success' => false, 'message' => "No file exists with ID $guid."));
+  trigger_error("No file exists with ID $guid.", E_ERROR);
+}
+
 ?>
